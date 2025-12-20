14ac3dafc3f0691bbb4632012cf16af512e46f7e
vivic04 <vihaanchugh2811@gmail.com>
2025-12-19 21:38:30 -0500

    remove encoding

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ DB_FILE = "price_history.json"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
 }
 
